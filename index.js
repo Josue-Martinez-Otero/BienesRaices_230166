e535f4d3fdb12ed2b3ef8a9033bc65de3e295e06
Josue A. <230166@utxicotepec.edu.mx>
2024-10-31 14:32:04 -0600

    Practica 11
    Se agrego pug y se instalo

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const router = express.Router();
 import express from 'express';
 import generalRoutes from './routers/generalRoutes.js'
 import userRoutes from './routers/userRoutes.js'
@@ -6,15 +7,25 @@ import userRoutes from './routers/userRoutes.js'
 // Instanciar nuestra aplicación web
 const app = express()
 
+//Habilitar PUG
+app.set('view engine','pug');
+app.set('views','./views')
+
 // Configuramos nuestro servidor web 
 const port = 3000;
 app.listen(port, ()=>{
-   console.log(`La aplicación ha iniciado en el puesto: ${port}`);
+   console.log(`La aplicación ha iniciado en el puerto: ${port}`);
 })
 
 //Routing - Enrutamiento.
 app.use('/',generalRoutes);
-app.use('/usuario',userRoutes);
-// Probamos las rutas para poder presentar mensajes al usuario a través del navegador
+app.use('/auth',userRoutes);
 
+// Probamos las rutas para poder presentar mensajes al usuario a través del navegador
+app.get("/",function(req,res){
+    res.send("Hola Mundo desde node, a traves del navegador")
+})
+app.use('/',generalRoutes)
+app.use('/auth',userRoutes)
+export default router;
 
